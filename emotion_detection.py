064b6cf5ef1005dd52107b42ca70af2e8ec477c9
katrikken <kurysheva.katerina@gmail.com>
2025-08-26 11:59:25 +0200

    Update emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -2,15 +2,32 @@ import requests
 import json
 
 def emotion_detector(text_to_analyze):
+    empty_response = {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+        }
+
+    if not text_to_analyze or text_to_analyze.strip() == "":
+        return empty_response
+
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
     headers = {
         "Content-Type": "application/json",
         "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
     }
-    input_json = {"raw_document": {"text": text_to_analyze}}
+    input_json = {"raw_document": {"text": text_to_analyze.strip()}}
 
     response = requests.post(url, headers=headers, json=input_json)
     response.raise_for_status()
+
+    # Handle blank input (status_code = 400)
+    if response.status_code == 400:
+        return empty_response
+
     result = response.json()
     emotions = result.get("emotionPredictions", [])[0].get("emotion", {})
 
@@ -38,12 +55,3 @@ def emotion_detector(text_to_analyze):
         "dominant_emotion": dominant_emotion
     }
 
-
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) < 2:
-        print("Usage: python emotion.py \"Your text to analyze\"")
-        sys.exit(1)
-
-    text_to_analyze = sys.argv[1]
-    print(emotion_detector(text_to_analyze))
